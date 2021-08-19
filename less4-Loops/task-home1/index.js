1d1aa484171fec0b2f8eabebcb4090b88fa862a7
vetal <vetalsoad@gmail.com>
2021-08-19 13:54:08 +0300

    ok

--- less4-Loops/task-home1/index.js
+++ less4-Loops/task-home1/index.js
@@ -1,15 +1,15 @@
 function run() {
-  const a = 0;
+  var a = 0;
 
-  if (a === 1) {
-    return a;
+  if (a == 1) {
+    return c;
   }
 
-  for (let i = 1; i < 10; i+1) {
+  for (let i = 1; i < 10; i++) {
     console.log(i);
   }
 
-  return a;
+  return (d = a);
 }
 
 run();

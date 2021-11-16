28077c67b89a1f3d384cbe4df54c4e8587f4b536
vetal <vetalsoad@gmail.com>
2021-11-16 16:52:00 +0200

    qwq

--- less-21-DOM/hw2/index.js
+++ less-21-DOM/hw2/index.js
@@ -1,44 +1,15 @@
-export const tasks = [
-  { text: "Buy milk", done: false },
-  { text: "Pick up Tom from airport", done: false },
-  { text: "Visit party", done: false },
-  { text: "Visit doctor", done: true },
-  { text: "Buy meat", done: true },
-];
+export function getItemsList() {
+  const elementsList = document.querySelectorAll(".technology");
+  console.dir(elementsList);
+  return elementsList;
+}
 
-/**
- * @param {object[]} tasksList
- * @return {undefined}
- */
-const renderTasks = (tasksList) => {
-  console.log(`renderTasks is running`);
+getItemsList();
 
-  const listElem = document.querySelector(".list");
+export function getItemsArray() {
+  const elementsArray = Array.from(document.querySelectorAll(".tool"));
+  console.dir(elementsArray);
+  return elementsArray;
+}
 
-  const listItemsElements = tasksList.map((item) => {
-    const listItemElement = document.createElement("li");
-    listItemElement.classList.add("list__item");
-
-    const checkbox = document.createElement("INPUT");
-    checkbox.setAttribute("type", "checkbox");
-    checkbox.classList.add("list__item-checkbox");
-
-    if (item.done) {
-      checkbox.checked = "true";
-      listItemElement.classList.toggle("list__item_done");
-    }
-
-    listItemElement.append(checkbox);
-    listItemElement.append(`${item.text}`);
-
-    return listItemElement;
-  });
-
-  listElem.append(...listItemsElements);
-};
-renderTasks(tasks);
-
-document.querySelector(".create-task-btn").onclick = () => {
-  const newValueText = document.querySelector(".task-input").value;
-  tasks.push({ text: newValueText, done: false });
-};
+getItemsArray();

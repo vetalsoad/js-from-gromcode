de5d64f95cd4c2400a078e7125a2a2f44b15789b
vetal <vetalsoad@gmail.com>
2021-12-10 19:46:59 +0200

    zxz

--- less-35-errorHandling/task3/index.js
+++ less-35-errorHandling/task3/index.js
@@ -1,52 +1,58 @@
-// algo
-// 1. пишу обработчик событий для кнопки:
-//          -при клике на кнопку, отображаю данные на странице
-//
-//
-//
-//
-//
-//
-//
-//
-//
-const userAvatarElem = document.querySelector(".user__avatar");
-const userNameElem = document.querySelector(".user__name");
-const userLocationElem = document.querySelector(".user__location");
-const repoListData = document.querySelector(".repo-list");
+import {
+  renderUserData,
+  renderRepos,
+  clearList,
+} from "./src/render.js";
+import { showSpinner, hideSpinner } from "./src/spinner.js";
+import { fetchUserData, fetchRepositories } from "./src/apiRequests.js";
 
-const linkDataURL = 'https://api.github.com/users';
-/* const linkRepoListDataURL = ''; */
+const defaultUserAvatar = "https://avatars3.githubusercontent.com/u10001";
+const defaultUser = {
+  avatar_url: defaultUserAvatar,
+  name: "",
+  location: "",
+};
+const userNameInput = document.querySelector(".name-form__input");
+const showUserBtnElem = document.querySelector(".name-form__btn");
 
+renderUserData(defaultUser);
 
+const onSearchUser = () => {
+  showSpinner();
+  const userName = userNameInput.value;
+  fetchUserData(userName)
+    .then((userData) => {
+      renderUserData(userData);
+      return userData.repos_url;
+    })
+    .then((url) => fetchRepositories(url))
+    .then((reposList) => {
+      renderRepos(reposList);
+      hideSpinner();
+    })
+    .catch((err) => {
+      alert(err.message);
+    })
+    .finally(() => {
+      hideSpinner();
+    });
+};
 
-const defaultAvatar = "https://avatars3.githubusercontent.com/u10001";
+showUserBtnElem.addEventListener("click", onSearchUser);
 
-userAvatarElem.src = defaultAvatar;
-
-const fetchUserData = userName => {
-  return fetch(`${linkDataURL}/${userName}`)
-    .then(responce => responce.json())
-}
-
-const renderUserData = userData => {
-  console.log(userData);
-  const {avatar_url, name, location, repos_url} = userData;
-  userAvatarElem.src = avatar_url;
-  userNameElem.textContent = name;
-  userLocationElem.textContent = location;
-  /* linkRepoListDataURL = repos_url; */
-  
-}
- 
 const showUserBtnElem = document.querySelector(".name-form__btn");
 const userNameInputElem = document.querySelector(".name-form__input");
 
 const onSearchUser = () => {
   const userName = userNameInputElem.value;
+
   fetchUserData(userName)
-    .then(userData => renderUserData(userData));
+    .then((userData) => {
+      renderUserData(userData);
+      console.log(userData.repos_url);
+      return userData.repos_url;
+    })
+    .then((url) => renderRepos(url));
 };
 
-
-showUserBtnElem.addEventListener('click', onSearchUser);
+showUserBtnElem.addEventListener("click", onSearchUser);

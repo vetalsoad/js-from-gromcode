c5a0709895f6b84ad68183e1f6d7fd5e630b83c0
vetal <vetalsoad@gmail.com>
2021-12-12 15:48:29 +0200

    eqe

--- less-35-errorHandling/task3/index.js
+++ less-35-errorHandling/task3/index.js
@@ -1,51 +1,6 @@
 import { showSpinner, hideSpinner } from "./scr/spinner.js";
-
-const userAvatarEl = document.querySelector(".user__avatar");
-const userNameEl = document.querySelector(".user__name");
-const userLocationEl = document.querySelector(".user__location");
-const repoListEl = document.querySelector(".repo-list");
-// https://api.github.com/users/USERNAME
-// https://avatars3.githubusercontent.com/u10001
-
-const defoultAvatar = "https://avatars3.githubusercontent.com/u10001";
-userAvatarEl.src = defoultAvatar;
-
-const fetchUserData = (userName) => {
-  return fetch(`https://api.github.com/users/${userName}`).then((response) => {
-    if (response.ok) {
-      return response.json();
-    }
-    throw new Error("Failed to load data");
-  });
-};
-
-const renderUserData = (userData) => {
-  const { avatar_url, name, location, repos_url } = userData;
-  userAvatarEl.src = avatar_url;
-  userNameEl.textContent = name;
-  userLocationEl.textContent = location;
-  return repos_url;
-};
-
-const fetchRepositories = (url) => {
-  return fetch(url).then((response) => {
-    if (response.ok) {
-      return response.json();
-    }
-    throw new Error("Failed to load data");
-  });
-};
-
-const renderRepos = (reposList) => {
-  const repoListElem = reposList.map(({ name }) => {
-
-    const listItemElem = document.createElement("li");
-    listItemElem.classList.add("repo-list__item");
-    listItemElem.textContent = name;
-    return listItemElem;
-  });
-  repoListEl.append(...repoListElem);
-};
+import { renderUserData, renderRepos } from "./scr/render.js";
+import { fetchUserData, fetchRepositories } from "./scr/apiRequest.js";
 
 const showUserBtnElem = document.querySelector(".name-form__btn");
 const userNameInputElem = document.querySelector(".name-form__input");
---
+++ less-35-errorHandling/task3/scr/render.js
@@ -0,0 +1,25 @@
+const userAvatarEl = document.querySelector(".user__avatar");
+const userNameEl = document.querySelector(".user__name");
+const userLocationEl = document.querySelector(".user__location");
+const repoListEl = document.querySelector(".repo-list");
+
+const defoultAvatar = "https://avatars3.githubusercontent.com/u10001";
+userAvatarEl.src = defoultAvatar;
+
+export const renderUserData = (userData) => {
+  const { avatar_url, name, location, repos_url } = userData;
+  userAvatarEl.src = avatar_url;
+  userNameEl.textContent = name;
+  userLocationEl.textContent = location;
+  return repos_url;
+};
+
+export const renderRepos = (reposList) => {
+  const repoListElem = reposList.map(({ name }) => {
+    const listItemElem = document.createElement("li");
+    listItemElem.classList.add("repo-list__item");
+    listItemElem.textContent = name;
+    return listItemElem;
+  });
+  repoListEl.append(...repoListElem);
+};
